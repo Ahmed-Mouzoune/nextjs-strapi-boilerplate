6d5a661fd6ccfd610f03b4a6d49dfdbf6204d298
AbderrahmaneMouzoune <abderrahmane.mouzoune@gmail.com>
2024-08-14 19:50:37 +0200

    feat(backend): :sparkles: Implement SEO Shared ContentType by strapi-seo-plugin

--- apps/backend/config/plugins.ts
+++ apps/backend/config/plugins.ts
@@ -1,1 +1,5 @@
-export default () => ({});
+export default () => ({
+  seo: {
+    enabled: true,
+  },
+});
--- apps/backend/types/generated/components.d.ts
+++ apps/backend/types/generated/components.d.ts
@@ -1,41 +1,94 @@
-import type { Schema, Attribute } from "@strapi/strapi";
+import type { Schema, Attribute } from '@strapi/strapi';
 
 export interface BlockButton extends Schema.Component {
-  collectionName: "components_block_buttons";
+  collectionName: 'components_block_buttons';
   info: {
-    displayName: "Button";
-    icon: "oneToMany";
+    displayName: 'Button';
+    icon: 'oneToMany';
   };
   attributes: {
     text: Attribute.String & Attribute.Required;
     link: Attribute.String & Attribute.Required;
-    size: Attribute.Enumeration<["default", "sm", "lg", "icon"]> &
+    size: Attribute.Enumeration<['default', 'sm', 'lg', 'icon']> &
       Attribute.Required &
-      Attribute.DefaultTo<"default">;
+      Attribute.DefaultTo<'default'>;
     variant: Attribute.Enumeration<
-      ["default", "destructive", "outline", "secondary", "ghost", "link"]
+      ['default', 'destructive', 'outline', 'secondary', 'ghost', 'link']
     > &
       Attribute.Required &
-      Attribute.DefaultTo<"default">;
+      Attribute.DefaultTo<'default'>;
   };
 }
 
 export interface BlockWysiwig extends Schema.Component {
-  collectionName: "components_block_wysiwigs";
+  collectionName: 'components_block_wysiwigs';
   info: {
-    displayName: "Wysiwig";
-    icon: "file";
+    displayName: 'Wysiwig';
+    icon: 'file';
   };
   attributes: {
     content: Attribute.RichText & Attribute.Required;
   };
 }
 
-declare module "@strapi/types" {
+export interface SharedMetaSocial extends Schema.Component {
+  collectionName: 'components_shared_meta_socials';
+  info: {
+    displayName: 'metaSocial';
+    icon: 'project-diagram';
+  };
+  attributes: {
+    socialNetwork: Attribute.Enumeration<['Facebook', 'Twitter']> &
+      Attribute.Required;
+    title: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    description: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 65;
+      }>;
+    image: Attribute.Media<'images' | 'files' | 'videos'>;
+  };
+}
+
+export interface SharedSeo extends Schema.Component {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+    icon: 'search';
+  };
+  attributes: {
+    metaTitle: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    metaDescription: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 50;
+        maxLength: 160;
+      }>;
+    metaImage: Attribute.Media<'images' | 'files' | 'videos'>;
+    metaSocial: Attribute.Component<'shared.meta-social', true>;
+    keywords: Attribute.Text;
+    metaRobots: Attribute.String;
+    structuredData: Attribute.JSON;
+    metaViewport: Attribute.String;
+    canonicalURL: Attribute.String;
+  };
+}
+
+declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      "block.button": BlockButton;
-      "block.wysiwig": BlockWysiwig;
+      'block.button': BlockButton;
+      'block.wysiwig': BlockWysiwig;
+      'shared.meta-social': SharedMetaSocial;
+      'shared.seo': SharedSeo;
     }
   }
 }
